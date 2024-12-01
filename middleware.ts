90283583aee830a89d39071b698154a27e9bf40c
Omar <44785357+feynman88@users.noreply.github.com>
2024-12-01 16:42:08 +0200

    clerk update fixed

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,13 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isWebhookRoute = createRouteMatcher(["/api/webhooks/clerk"]);
-const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks/clerk",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
-  if (isAdminRoute(request)) {
-    await auth.protect();
-  }
-
-
-  if (!isWebhookRoute(request)) {
+  if (!isPublicRoute(request)) {
     await auth.protect();
   }
 });
