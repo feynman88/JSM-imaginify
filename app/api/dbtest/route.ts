95253366f0ccd65fb4cff3c7955a01c6c84612a3
Omar <44785357+feynman88@users.noreply.github.com>
2024-12-02 18:11:31 +0200

    webhook wild card

---
+++ app/api/dbtest/route.ts
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+import { createUser } from "@/lib/actions/user.actions";
+
+const user = {
+  clerkId: "213s",
+  email: "omar.assem.1913@gmail.com",
+  username: "omarusername",
+  firstName: "omar",
+  lastName: "omar",
+  photo: "image.png",
+};
+
+await createUser(user);
+
